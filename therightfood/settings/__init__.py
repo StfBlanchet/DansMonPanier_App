c207f9d29794a810f6136cd3f892bce159a405fe
StfBlanchet <47082711+StfBlanchet@users.noreply.github.com>
2020-01-25 19:51:25 +0100

    _

--- therightfood/settings/__init__.py
+++ therightfood/settings/__init__.py
@@ -9,7 +9,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '!wa*gya13(a2#tip%cd8v_vb^_(a51f$+mpby4o&9g3y11n_6(')')
+SECRET_KEY = os.environ.get('SECRET_KEY', '!wa*gya13(a2#tip%cd8v_vb^_(a51f$+mpby4o&9g3y11n_6(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
