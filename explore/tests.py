fc8242e05e069cdcc1a9ca8b0b9fd169d794a3a6
StfBlanchet <47082711+StfBlanchet@users.noreply.github.com>
2020-02-06 14:40:45 +0100

    Tests refactoring and user stories

--- explore/tests.py
+++ explore/tests.py
@@ -3,59 +3,95 @@ dansMonPanier app
 File to run tests.
 """
 
-
-from .views import *
-from .models import *
-from .forms import UserForm
+from explore.views import *
+from explore.models import *
+from explore.forms import UserForm
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
 
-class SimpleViewTest(TestCase):
+class ViewTestNoRedir(TestCase):
     """
-    This class performs simple view tests
-    through status code and pieces of content
+    This class performs view tests
+    with status code 200 expected
+    and pieces of content check
     """
+
     def test_index(self):
+        """
+        The user accesses the index page
+        and sees the main headline
+        """
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Je sais ce que je mange !")
+        self.assertContains(response, 'Je sais ce que je mange !')
 
     def test_legal(self):
+        """
+        The user accesses the legal page
+        and sees the main headline
+        """
         response = self.client.get(reverse('legal'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Directeur de publication")
+        self.assertContains(response, 'Directeur de publication')
 
     def test_signin(self):
+        """
+        The user accesses the signin page
+        and sees the main headline
+        """
         response = self.client.get(reverse('signin'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Créez votre espace")
+        self.assertContains(response, 'Créez votre espace')
 
     def test_user_login(self):
+        """
+        The user accesses the signin page
+        and sees the main headline
+        """
         response = self.client.get(reverse('user_login'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Accédez à votre espace")
+        self.assertContains(response, 'Accédez à votre espace')
 
-    def test_user_logout(self):
-        response = self.client.get(reverse('user_logout'))
+
+class ViewTestRedir(TestCase):
+    """
+    This class performs view tests
+    with status code 302 expected
+    """
+
+    def test_my_profile(self):
+        """
+        Unauthenticated user can't access
+        profile page, hence the redirection
+        """
+        response = self.client.get(reverse('my_profile'))
         self.assertEqual(response.status_code, 302)
 
     def test_my_favorites(self):
+        """
+        Unauthenticated user can't access
+        favorites page, hence the redirection
+        """
         response = self.client.get(reverse('my_favorites'))
         self.assertEqual(response.status_code, 302)
 
     def test_save_favorites(self):
+        """
+        Unauthenticated user can't save
+        products, hence the redirection
+        """
         response = self.client.get(reverse('save_favorites'))
         self.assertEqual(response.status_code, 302)
 
     def test_remove_favorites(self):
+        """
+        Unauthenticated user can't remove
+        products, hence the redirection
+        """
         response = self.client.get(reverse('remove_favorites'))
-        self.assertEqual(response.status_code, 302)
-
-    def test_my_profile(self):
-        response = self.client.get(reverse('my_profile'))
         self.assertEqual(response.status_code, 302)
 
 
@@ -75,11 +111,6 @@ class AppDataTestCase(TestCase):
         Category.objects.create(
             id=1,
             category='Beurres'
-        )
-
-        Category.objects.create(
-            id=2,
-            category='Choux'
         )
 
         Food.objects.create(
@@ -118,7 +149,7 @@ class AppDataTestCase(TestCase):
             salt=2.0,
             fiber=10.0,
             proteins=66.0
-            )
+        )
 
         Food.objects.create(
             id=1,
@@ -156,7 +187,7 @@ class AppDataTestCase(TestCase):
             salt=2.0,
             fiber=1.0,
             proteins=26.0
-            )
+        )
 
         User.objects.create_user(
             id=0,
@@ -166,8 +197,10 @@ class AppDataTestCase(TestCase):
             first_name='Testeur@MonPanier')
 
         Favorite.objects.create(
+            id=0,
             products=Food.objects.get(pk=0),
-            user=User.objects.get(pk=0)
+            user=User.objects.get(pk=0),
+            meal='petit_dejeuner'
         )
 
     def setUp(self):
@@ -175,7 +208,10 @@ class AppDataTestCase(TestCase):
         Set working variables
         """
         self.factory = RequestFactory()
+        # Authenticated active user
         self.user = User.objects.get(username='testeur_monpanier@gmail.com')
+        self.client.login(username='testeur_monpanier@gmail.com', password='!MonP@nier:159357$')
+        # New users
         self.new_user = {
             'email': 'jeanpaul-dubuc@gmail.com',
             'username': 'jeanpaul-dubuc@gmail.com',
@@ -191,13 +227,182 @@ class AppDataTestCase(TestCase):
             'first_name': 'Toto'
         }
 
-    def test_food_str(self):
-        food = Food(name="Fake Name", stores="Fake stores", category_group="Test")
-        self.assertIs(food.__str__(), "Test")
+    """
+    Test auth scenarios
+    """
 
-    def test_cat_str(self):
-        category = Category(category="Test")
-        self.assertIs(category.__str__(), "Test")
+    def test_user_login(self):
+        """
+        The user logs in
+        """
+        user_is_logged = self.client.login(username='testeur_monpanier@gmail.com', password='!MonP@nier:159357$')
+        self.assertTrue(user_is_logged)
+
+    def test_my_profile_auth(self):
+        """
+        Authenticated user accesses her profile
+        which entails a specific context
+        (user.username, user.first_name)
+        """
+        request = self.factory.get(reverse('my_profile'))
+        request.user = self.user
+        response = my_profile(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'testeur_monpanier@gmail.com')
+        self.assertContains(response, 'Testeur@MonPanier')
+
+    def test_item(self):
+        """
+        The user accesses the page of
+        a given product and sees its features
+        """
+        request = self.factory.get('/item/1/')
+        request.user = self.user
+        response = item(request, 1)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Butter Test")
+        self.assertContains(response, "Fake Butter Brand")
+        self.assertContains(response, "Leclerc")
+
+    def test_my_favorites_auth(self):
+        """
+        Authenticated user accesses her favorites
+        and sees the headline, and the total of
+        registered products
+        """
+        request = self.factory.get(reverse('my_favorites'))
+        request.user = self.user
+        response = my_favorites(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Mes favoris')
+        user_favorites = Favorite().ref_list(request)
+        self.assertEqual(len(user_favorites), 1)
+
+    def test_save_favorites_auth(self):
+        """
+        Authenticated user saves a new favorite
+        and the total of registered products
+        increases (1 -> 2)
+        """
+        request = self.factory.get(reverse('save_favorites'))
+        request.user = self.user
+        product = Food.objects.get(pk=1)
+        m = 'petit_dejeuner'
+        Favorite(id=1, user=request.user, products=product, meal=m).save()
+        user_favorites = Favorite().ref_list(request)
+        self.assertEqual(len(user_favorites), 2)
+
+    def test_remove_favorites_auth(self):
+        """
+        Authenticated user saves a new favorite
+        and the total of registered products
+        decreases (2 -> 1)
+        """
+        request = self.factory.get(reverse('remove_favorites'))
+        request.user = self.user
+        Favorite(id=1, user=request.user).delete()
+        user_favorites = Favorite().ref_list(request)
+        self.assertEqual(len(user_favorites), 1)
+
+    def test_user_logout(self):
+        """
+        The user logs out and is
+        redirected to the index page
+        """
+        response = self.client.get(reverse('user_logout'))
+        self.assertEqual(response.status_code, 302)
+
+    """
+    Test signin scenarios
+    """
+
+    def test_new_valid_user(self):
+        """
+        A new valid user signs in and
+        the total of users increases
+        """
+        initial_count = User.objects.count()
+        form = UserForm(data=self.new_user)
+        self.assertTrue(form.is_valid())
+        form.save()
+        update_count = User.objects.count()
+        self.assertEqual(update_count, initial_count + 1)
+
+        # The new user can log in
+        user = User.objects.get(first_name='Je@anP@ul')
+        self.assertEqual(user.username, 'jeanpaul-dubuc@gmail.com')
+        user_is_logged = self.client.login(username='jeanpaul-dubuc@gmail.com', password='$123a456Cv789p!')
+        self.assertTrue(user_is_logged)
+
+        # Expected status response is 200
+        response = self.client.post(reverse('user_login'), data=self.new_user)
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_bad_user(self):
+        """
+        A new user tries to register
+        using non-compliant entries
+        """
+        form = UserForm(data=self.bad_user)
+        self.assertFalse(form.is_valid())
+        user_is_logged = self.client.login(username='toto@gmail.com', password='1234')
+        self.assertFalse(user_is_logged)
+
+    """
+    Test search results
+    """
+
+    def test_results(self):
+        """
+        The user generates a query that
+        combines ranking and filtering
+        for an existing category
+        """
+        uris = ['/results/?q=beurre&ranking=nutrition&filtering=none',
+                '/results/?q=beurre&ranking=nova&filtering=none',
+                '/results/?q=beurre&ranking=nova&filtering=bio',
+                '/results/?q=beurre&ranking=nova&filtering=fair-trade',
+                '/results/?q=beurre&ranking=nutrition&filtering=made-in-france',
+                '/results/?q=beurre&ranking=nutrition&filtering=palm-oil-free',
+                '/results/?q=beurre&ranking=nutrition&filtering=fsc']
+        data = Food.objects.filter(category_group__unaccent__contains='Beurre')
+        for uri in uris:
+            request = self.factory.get(uri)
+            request.user = self.user
+            response = results(request)
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, 'Faites le bon choix !')
+            self.assertIs(len(data), 1)
+
+    def test_no_results(self):
+        """
+        The user generates a query
+        for a category that does not
+        exist in the db
+        """
+        request = self.factory.get('/results/?q=chou&ranking=nutrition&filtering=none')
+        request.user = self.user
+        data = Food.objects.filter(category_group__unaccent__contains='Chou')
+        self.assertIs(len(data), 0)
+
+    def test_no_results_long_query(self):
+        request = self.factory.get('/results/?q=gateau+au+chocolat&ranking=nutrition&filtering=none')
+        request.user = self.user
+        data = Food.objects.filter(category_group__unaccent__contains='Pain au chocolat')
+        self.assertIs(len(data), 0)
+
+    """
+    Test models
+    """
+
+    def test_model_str(self):
+        """
+        Verify model strings
+        """
+        food = Food(name='Fake Name', stores='Fake stores', category_group='Test group')
+        self.assertIs(food.__str__(), 'Test group')
+        category = Category(category='Test category')
+        self.assertIs(category.__str__(), 'Test category')
 
     def test_item_count(self):
         """
@@ -217,185 +422,3 @@ class AppDataTestCase(TestCase):
         """
         product = Food.objects.get(name='Butter Test')
         self.assertIs(product.nova, 2)
-
-    def test_item(self):
-        """
-        Test item view regarding
-        status and returned data
-        with auth user
-        """
-        request = self.factory.get('/item/1/')
-        request.user = self.user
-        response = item(request, 1)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Butter Test")
-        self.assertContains(response, "Fake Butter Brand")
-        self.assertContains(response, "Leclerc")
-        user_favorites = Favorite().ref_list(request)
-        self.assertEqual(len(user_favorites), 1)
-
-    def test_new_user_1(self):
-        """
-        Test signin
-        for a valid user
-        """
-        response = self.client.post(reverse('signin'), data=self.new_user)
-        self.assertEqual(response.status_code, 302)
-
-    def test_new_user_2(self):
-        """
-        Test signin form and login
-        for a valid user
-        """
-        initial_count = User.objects.count()
-
-        form = UserForm(data=self.new_user)
-        self.assertTrue(form.is_valid())
-        form.save()
-
-        update_count = User.objects.count()
-        self.assertEqual(update_count, initial_count + 1)
-
-        user = User.objects.get(first_name='Je@anP@ul')
-        self.assertEqual(user.username, 'jeanpaul-dubuc@gmail.com')
-
-        user_is_logged = self.client.login(username='jeanpaul-dubuc@gmail.com', password='$123a456Cv789p!')
-        self.assertTrue(user_is_logged)
-
-    def test_auth_nok(self):
-        """
-        Test signin form and login
-        for a not valid user
-        """
-        form = UserForm(data=self.bad_user)
-        self.assertFalse(form.is_valid())
-
-        user_is_logged = self.client.login(username='toto@gmail.com', password='1234')
-        self.assertFalse(user_is_logged)
-
-    def test_auth_user_ok(self):
-        """
-        Test login
-        for a valid user
-        """
-        response = self.client.post(reverse('user_login'), data=self.new_user)
-        self.assertEqual(response.status_code, 200)
-
-    def test_my_profile_auth(self):
-        """
-        Test the access to the profile page
-        by an auth user and
-        verify returned data
-        """
-        request = self.factory.get(reverse('my_profile'))
-        request.user = self.user
-        response = my_profile(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "testeur_monpanier@gmail.com")
-
-    def test_checking_user_favorites(self):
-        """
-        Check favorite list content
-        """
-        request = self.factory.get(reverse('my_favorites'))
-        request.user = self.user
-        favorites = Food.objects.filter(favorite__user=request.user)
-        response = my_favorites(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertIsNot(len(favorites), 0)
-
-    def test_save_favorites_auth(self):
-        """
-        Test save_favorites
-        response
-        """
-        product = Food.objects.get(pk=1)
-        response = self.client.post(reverse('save_favorites'), {'products': product, 'user': self.user})
-        self.assertEqual(response.status_code, 302)
-
-    def test_favorites_count(self):
-        """
-        Test favorites count
-        is incremented when a
-        product was added
-        """
-        request = self.factory.get(reverse('save_favorites'))
-        request.user = self.user
-        initial_count = Favorite.objects.count()
-        new = Food.objects.get(pk=1)
-        Favorite(products=new, user=request.user).save()
-        update_count = Favorite.objects.count()
-        self.assertEqual(update_count, initial_count + 1)
-
-    def test_remove_favorites_auth(self):
-        request = self.factory.get(reverse('my_favorites'))
-        request.user = self.user
-        favorites = Food.objects.filter(favorite__user=request.user)
-        Favorite.objects.filter(Q(products=1) & Q(user=request.user)).delete()
-        self.assertIs(len(favorites), 1)
-
-    def test_results_nutrition(self):
-        request = self.factory.get('/results/?q=beurre&ranking=nutrition&filtering=none')
-        request.user = self.user
-        response = results(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_results_nova(self):
-        request = self.factory.get('/results/?q=beurre&ranking=nova&filtering=none')
-        data = Food.objects.filter(category_group__unaccent__contains='Beurre')
-        request.user = self.user
-        response = results(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertIs(len(data), 1)
-
-    def test_results_nova_bio(self):
-        request = self.factory.get('/results/?q=beurre&ranking=nova&filtering=bio')
-        data = Food.objects.filter(category_group__unaccent__contains='Beurre')
-        request.user = self.user
-        response = results(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertIs(len(data), 1)
-
-    def test_results_nova_fair(self):
-        request = self.factory.get('/results/?q=pain&ranking=nova&filtering=fair-trade')
-        data = Food.objects.filter(category_group__unaccent__contains='Pain')
-        request.user = self.user
-        response = results(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertIs(len(data), 1)
-
-    def test_results_nutrition_france(self):
-        request = self.factory.get('/results/?q=pain&ranking=nutrition&filtering=made-in-france')
-        data = Food.objects.filter(category_group__unaccent__icontains='Pain')
-        request.user = self.user
-        response = results(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertIs(len(data), 1)
-
-    def test_results_nutrition_palm(self):
-        request = self.factory.get('/results/?q=pain&ranking=nutrition&filtering=palm-oil-free')
-        data = Food.objects.filter(category_group__unaccent__icontains='Pain')
-        request.user = self.user
-        response = results(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertIs(len(data), 1)
-
-    def test_results_nutrition_fsc(self):
-        request = self.factory.get('/results/?q=pain&ranking=nutrition&filtering=fsc')
-        data = Food.objects.filter(category_group__unaccent__icontains='Pain')
-        request.user = self.user
-        response = results(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertIs(len(data), 1)
-
-    def test_results_not_in_cat_1(self):
-        request = self.factory.get('/results/?q=chou&ranking=nutrition&filtering=none')
-        request.user = self.user
-        data = Food.objects.filter(category_group__unaccent__icontains='Chou')
-        self.assertIs(len(data), 0)
-
-    def test_results_not_in_cat_2(self):
-        request = self.factory.get('/results/?q=gateau+au+chocolat&ranking=nutrition&filtering=none')
-        request.user = self.user
-        data = Food.objects.filter(category_group__unaccent__icontains='Gateau au chocolat')
-        self.assertIs(len(data), 0)
